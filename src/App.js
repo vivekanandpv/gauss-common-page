8071dc45667696b5ea20fe001bdf919dfced56c4
Vivekanand P V <vivekanandpv@outlook.com>
2020-10-25 22:31:23 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './Styles.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <div>
+        <div className='row full-height full-width'>
+          <div className='col-md-6 col-sm-12 full-height'>
+            <div className='d-flex align-items-end justify-content-end w-100'>
+              <div className='p-3 text-right'>
+                <h3 className='title'>Princeps mathematicorum</h3>
+                <h2 className='header-text'>Johann Carl Friedrich Gauss</h2>
+              </div>
+            </div>
+          </div>
+          <div className='col-md-6 col-sm-12 full-height'>
+            <div className='d-flex flex-column align-items-center justify-content-center w-100'>
+              <div className='w-50 my-3 logo-holder'></div>
+              <div
+                className='card my-3 w-75 link-box'
+                onClick={() => window.open('http://localhost:3001', '_blank')}
+              >
+                <div className='card-body'>
+                  <h4 className='title'>Credit Cards</h4>
+                </div>
+              </div>
+              <div
+                className='card my-3 w-75 link-box'
+                onClick={() => window.open('http://localhost:3002', '_blank')}
+              >
+                <div className='card-body'>
+                  <h4 className='title'>Liquidity</h4>
+                </div>
+              </div>
+              <div
+                className='card my-3 w-75 link-box'
+                onClick={() => window.open('http://localhost:3003', '_blank')}
+              >
+                <div className='card-body'>
+                  <h4 className='title'>Payment Initiation</h4>
+                </div>
+              </div>
+              <div
+                className='card my-3 w-75 link-box'
+                onClick={() => window.open('http://localhost:3004', '_blank')}
+              >
+                <div className='card-body'>
+                  <h4 className='title'>RM Planner</h4>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
   );
-}
+};
 
 export default App;
